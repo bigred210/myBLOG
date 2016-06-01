577f784305e153c7224d9915b33493cfc41b2626
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-06-01 11:16:52 -0500

    created a view called roll-dice

---
+++ app/views/roll-dice.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Roll Dice View</title>
+</head>
+<body>
+    <h1>Hello, Welcome to my Roll Dice view!</h1>
+        <p>Your guess is : <?php echo $guess; ?> .</p>
+        <p>The roll equals = <?php echo $roll; ?> !</p>
+        <p><?php echo $message; ?></p>
+</body>
+</html>
