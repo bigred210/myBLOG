b21c1e68605c28c2b27496434548b8ed92595514
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-06-01 11:51:39 -0500

    changed around the tags surrounding my php echo statements from h3 to p

--- app/views/roll-dice.php
+++ app/views/roll-dice.php
@@ -5,8 +5,8 @@
 </head>
 <body>
     <h1>Hello, Welcome to my Roll Dice view!</h1>
-        <p>Your guess is : <?php echo $guess; ?> .</p>
-        <p>The roll equals = <?php echo $roll; ?> !</p>
-        <p><?php echo $message; ?></p>
+        <p>Your guess is : <?php echo $guess; ?>!</p>
+        <p>The roll equals = <?php echo $roll; ?>!</p>
+        <h3><?php echo $message; ?></h3>
 </body>
 </html>
