a2461d2324fce36c1e75348368cd930dc16c452c
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-06-01 11:18:27 -0500

    created a page called my first view

---
+++ app/views/my-first-view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My First View</title>
+</head>
+<body>
+    <h1>Hello, Welcome to my first view!</h1>
+        <p><?php echo $name; ?></p>
+</body>
+</html>
