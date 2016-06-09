ea29a5a753cea4c45cfce5797eff71033bc27218
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-06-09 16:44:14 -0500

    added if statement to show edit button only if loggged into the site.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -7,21 +7,38 @@
 
 @section('content')
     @include("posts.postsNav")
-    <div class="container">
-        <div class="row">
-            <div class="show col-sm-12 col-sm-4 col-sm-offset-2">
-                <h1>{{{ $post->title }}}</h1>
-                <p>Written by: {{{ $post->user->username }}}</p>
-                <p>{{{ $post->body }}}</p>
-                <p><small> - Posted on : {{$post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A')}};</small></p>
-                <p><small> - Updated on : {{$post->updated_at->setTimezone('America/Chicago')->diffForHumans()}};</small></p>
-                <div>
-                    <a href="{{{action('PostsController@edit', $post->id)}}}" class="btn btn-custom navbut">Edit</a>
+    @if(Auth::check())
+        <div class="container">
+            <div class="row">
+                <div class="show col-sm-12 col-sm-4 col-sm-offset-2">
+                    <h1>{{{ $post->title }}}</h1>
+                    <p>Written by: {{{ $post->user->username }}}</p>
+                    <p>{{{ $post->body }}}</p>
+                    <p><small> - Posted on : {{$post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A')}};</small></p>
+                    <p><small> - Updated on : {{$post->updated_at->setTimezone('America/Chicago')->diffForHumans()}};</small></p>
+                    <div>
+                        <a href="{{{action('PostsController@edit', $post->id)}}}" class="btn btn-custom navbut">Edit</a>
+                    </div>
+                </div> <!-- end of col -->
+                <div class="logo col-sm-4 col-sm-offset-1">
+                    <img src="/../img/posts.png">
                 </div>
-            </div> <!-- end of col -->
-            <div class="logo col-sm-4 col-sm-offset-1">
-                <img src="/../img/posts.png">
-            </div>
-        </div> <!-- end of row -->
-    </div> <!-- end of container -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    @else
+        <div class="container">
+            <div class="row">
+                <div class="show col-sm-12 col-sm-4 col-sm-offset-2">
+                    <h1>{{{ $post->title }}}</h1>
+                    <p>Written by: {{{ $post->user->username }}}</p>
+                    <p>{{{ $post->body }}}</p>
+                    <p><small> - Posted on : {{$post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A')}};</small></p>
+                    <p><small> - Updated on : {{$post->updated_at->setTimezone('America/Chicago')->diffForHumans()}};</small></p>
+                    </div> <!-- end of col -->
+                    <div class="logo col-sm-4 col-sm-offset-1">
+                        <img src="/../img/posts.png">
+                    </div>
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    @endif
 @stop
